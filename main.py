d74930ad9e5803ea6406299c4b7e9c988a1703c6
Andrew Kirov <AndrewKirov@me.com>
2023-02-16 22:58:57 +0500

    Add rectangle and bg color

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import sys
 import pygame
 
+
+screen_width, screen_height = 800, 600
+
 pygame.init()
-pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("My Pygame")
+
+
+rect_width, rect_height = 100, 200
+rect_x = screen_width / 2 - rect_width / 2
+rect_y = screen_height / 2 - rect_height / 2
+rect_color = pygame.Color('lightyellow')
+
 
 while True:
     for event in pygame.event.get():
         print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((32, 52, 71))
+    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
+    pygame.display.update()
+
